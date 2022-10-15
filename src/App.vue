4ef4806057ab520527206cb777aaa213a1868348
Zein <zeinmayhoub2@gmail.com>
2022-10-15 21:12:53 +0300

    All files have been added correctly

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PassGen />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PassGen from "./components/PassGen.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PassGen,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: "Montserrat", sans-serif;
+}
+body {
+  background-image: url(../src/imgs/bg.svg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
